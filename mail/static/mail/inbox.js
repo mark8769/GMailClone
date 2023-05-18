1f927a88d7771526415b5b54dede7321ff62d07c
Mark Ortega <mortegaponce27@gmail.com>
2023-05-17 22:31:31 -0500

    mail/static/inbox.js: Added fetch post request to send email.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  // Use buttons to toggle between views
+// Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.getElementById('submit-compose').addEventListener('click', sendEmail);
   // By default, load the inbox
   load_mailbox('inbox');
 });
-
+  
 function compose_email() {
-
+  console.log("compose email nav clicked.");
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -30,4 +29,42 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function sendEmail(){
+
+  console.log("clicked");
+  /* Prevent form from submitting right away, let django handle routing.
+  If we submit right away, fetch wont work since it is asynchronous.
+  It returns a promise that it will give you back the information at some point,
+  but doensn't know when. So if it happens before reload = no error. If it gets returned
+  after reloading the page (and reloading all sources again) then you will get an error.
+  */ 
+  // https://stackoverflow.com/questions/57240628/how-to-make-a-button-call-a-function-that-uses-the-fetch-api
+  //event.preventDefault();
+  // using type button instead of submit
+  //load_mailbox("sent");
+  // console.log("going home");
+  let recipients = document.getElementById("compose-recipients").value;
+  let subject = document.getElementById("compose-subject").value;
+  let body = document.getElementById("compose-body").value;
+
+  console.log("sending request");
+  fetch("http://127.0.0.1:8000/emails",{
+    method: "POST",
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json()) // no curly braces because returning right away
+  .then(result => {
+    console.log(result);
+    console.log("done");
+    console.log("loading sent mailbox");
+    load_mailbox("sent");
+  }); // curly braces, because not returning console.log
+  // see if code is working, refreshes too fast
+  //time.sleep(3);
 }
